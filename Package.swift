2c52914c4ff1152c03f33d482dfbbc60c3aef8a3
Brandy Carney <brandy@ionic.io>
2024-02-16 14:37:40 -0500

    style: rename about me app to kitchen sink app

--- Package.swift
+++ Package.swift
@@ -8,13 +8,13 @@ import PackageDescription
 import AppleProductTypes
 
 let package = Package(
-    name: "About Me",
+    name: "iOS Kitchen Sink",
     platforms: [
         .iOS("15.2")
     ],
     products: [
         .iOSApplication(
-            name: "About Me",
+            name: "iOS Kitchen Sink",
             targets: ["App"],
             displayVersion: "1.0",
             bundleVersion: "1",
